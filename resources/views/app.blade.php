1efc5c1b9f0f58a2531e6e64ff6f7a3e0479ca28
Ahmed Raza <ahmed.raza@square63.com>
2017-05-14 13:33:50 +0500

    Installed Laravel 5.4.23

---
+++ resources/views/app.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Karz and Bikes</title>
+</head>
+<body>
+  <h1>Welcome</h1>
+  <p>Lorem ipsum duis dolore proident occaecat officia cillum esse deserunt irure irure velit reprehenderit non.</p>
+</body>
+</html>

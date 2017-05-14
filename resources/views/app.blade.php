d8acf8115e1d43fc06c0105083b1d0a70e76693b
Ahmed Raza <ahmed.raza@square63.com>
2017-05-14 16:46:21 +0500

    CSRF Token Protection

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>{!! config('vars.site_name') !!} | {!! config('vars.slogan') !!}</title>
 </head>
 <body>

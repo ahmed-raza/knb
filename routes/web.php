12004aa376d0adeb36b241168e962a88d0e4f37d
Ahmed Raza <ahmed.raza@square63.com>
2017-05-15 13:13:37 +0500

    Removed profile route

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,6 @@ Route::get('/', ['as'=>'home', 'uses'=>'PagesController@index']);
 
 Route::group(['prefix'=>'user'], function(){
   Route::get('{id}', ['as'=>'user.show', 'uses'=>'UsersController@show']);
-  Route::get('profile', ['as'=>'user.profile', 'uses'=>'UsersController@profile']);
   Route::get('logout', ['as'=>'user.logout', 'uses'=>'UsersController@logout']);
 });
 

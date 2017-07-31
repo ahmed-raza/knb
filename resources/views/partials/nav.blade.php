556924ba81628987b188cae26f21ae94903ac9fb
Alex Roye <alex@creativefaze.com>
2017-07-31 17:34:16 +0500

    Linked ads route in navbar

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -15,6 +15,7 @@
   <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
     <ul class="nav navbar-nav">
       <li class="{{ Request::is('about') ? 'active' : '' }}"><a href="/about">About</a></li>
+      <li class="{{ Request::is('ads') ? 'active' : '' }}"><a href="/ads">Ads</a></li>
     </ul>
     <ul class="nav navbar-nav navbar-right">
       <li><a href="{{ route('ads.create') }}">Create ad</a></li>

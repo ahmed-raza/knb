28fdd382a37a3227a0d3b9109ba8d3e7ab268f52
Alex Roye <alex@creativefaze.com>
2017-08-01 17:17:05 +0500

    Ad middleware check ownership of ad

--- app/Http/Middleware/Ad.php
+++ app/Http/Middleware/Ad.php
@@ -17,14 +17,11 @@ class Ad
      */
     public function handle($request, Closure $next)
     {
-        $id = explode("/", Request::url())[4];
-        $ad = \App\Ad::findOrFail($id);
-        if (Auth::check()) {
-            if (Auth::user()->id === $ad->user_id) {
-                return $next($request);
-            }
-            return redirect('ads/'.$id)->with('error', 'You don\'t own that ad.');
+        $id = $request->route()->parameters();
+        $ad = \App\Ad::findOrFail($id['ad']);
+        if ((Auth::check() && Auth::user()->id === $ad->user->id) || (Auth::check() && Auth::user()->isAdmin())) {
+            return $next($request);
         }
-        return redirect('login')->with('error', 'You need to login first.');
+        return response()->view('errors.403');
     }
 }

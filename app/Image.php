92dcf613f850442176d950384fca15bacf61b9fc
Alex Roye <alex@creativefaze.com>
2017-08-01 10:19:03 +0500

    Fillable values in image entity

--- app/Image.php
+++ app/Image.php
@@ -6,6 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Image extends Model
 {
+
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array
+   */
+  protected $fillable = [
+    'ad_id', 'imageName', 'imagePath',
+  ];
   public function ads() {
     return $this->belongsToMany('App\Ad');
   }

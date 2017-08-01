226de936b7d6f7a8e02a24fc595bcb4eb1fc4be2
Alex Roye <alex@creativefaze.com>
2017-08-01 14:48:43 +0500

    Ads middleware definition and registration

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -60,5 +60,6 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'profile' => \App\Http\Middleware\Profile::class,
+        'ad' => \App\Http\Middleware\Ad::class,
     ];
 }

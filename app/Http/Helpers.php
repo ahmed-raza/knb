e188669cfa5fc773b3101791934be6951a68bc52
Alex Roye <alex@creativefaze.com>
2017-08-01 12:44:38 +0500

    Paths for thumbnail and actual image

--- app/Http/Helpers.php
+++ app/Http/Helpers.php
@@ -5,8 +5,17 @@ namespace App\Http;
 use Storage;
 
 class CustomHelper {
-  public static function getPath($image){
-    $image_path = Storage::get('ads/'.$image->ad_id.'/thumb_'.$image->imageName);
-    return $image_path;
+  public static function getPath($image, $type){
+    switch ($type) {
+      case 'thumb':
+        $image_path = Storage::get('ads/'.$image->ad_id.'/thumb_'.$image->imageName);
+        return $image_path;
+        break;
+      
+      default:
+        $image_path = Storage::get('ads/'.$image->ad_id.'/'.$image->imageName);
+        return $image_path;
+        break;
+    }
   }
 }

7a52352811adae0cf5ea99af442f0dac99a410dd
Alex Roye <alex@creativefaze.com>
2017-08-01 12:45:12 +0500

    Ads index thumbnail images

--- resources/views/ads/index.blade.php
+++ resources/views/ads/index.blade.php
@@ -8,7 +8,7 @@
   @foreach($ads as $ad)
     <div class="row">
       <div class="col-lg-4">
-        <img src="data:image/jpeg;base64,{{ base64_encode(CustomHelper::getPath($ad->images[0])) }}" alt="Ad Image">
+        <img src="data:image/jpeg;base64,{{ base64_encode(CustomHelper::getPath($ad->images[0], 'thumb')) }}" alt="Ad Image">
       </div>
       <div class="col-lg-8">
         <h2><a href="{{ route('ads.show', $ad->id) }}">{{ $ad->title }}</a></h2>

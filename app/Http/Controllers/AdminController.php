67308a6cd5c117594615f1ecb707300e699ac4e5
Alex Roye <alex@creativefaze.com>
2017-08-02 11:58:22 +0500

    Admin controller with dashboard and operations methods

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Ad;
+use App\User;
+use App\Image;
+use Validator;
+use Storage;
+use DB;
+
+class AdminController extends Controller
+{
+
+  public function __construct(){
+    return $this->middleware('admin');
+  }
+  public function dashboard(){
+    $ads = Ad::all();
+    $users = User::all();
+    return view('admin.dashboard', compact('ads', 'users'));
+  }
+  public function adsOperations(Request $request){
+    $rules = ['id' => 'required'];
+    $input = ['id' => $request->input('id')];
+    $validator = Validator::make($input, $rules)->passes();
+    if ($validator) {
+      switch ($request->input('bulk_actions')) {
+        case 'delete':
+        Ad::destroy($request->input('id'));
+        Image::where('ad_id', $request->input('id'))->delete();
+        return redirect('admin/dashboard')->with('message','Ads deleted');
+        break;
+        case 'publish':
+        foreach ($request->get('id') as $id) {
+          DB::table('ads')->where('id', $id)->update(['status' => 1]);
+        }
+        return redirect('admin/dashboard')->with('message','Ads Published');
+        break;
+        case 'unpublish':
+        foreach ($request->get('id') as $id) {
+          DB::table('ads')->where('id', $id)->update(['status' => 0]);
+        }
+        return redirect('admin/dashboard')->with('message','Ads Unpublished');
+        break;
+      }
+    } else {
+        return redirect('admin/dashboard')->with('error','No item selected.');
+    }
+  }
+}

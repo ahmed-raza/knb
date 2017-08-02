55bd696182b0055c4dfb834d94b074a4404b3088
Alex Roye <alex@creativefaze.com>
2017-08-02 10:38:22 +0500

    Edit Button text updated

--- resources/views/ads/edit.blade.php
+++ resources/views/ads/edit.blade.php
@@ -6,7 +6,7 @@
   <h1>Edit: {!! HTML::link('ads/'.$ad->id, $ad->title) !!}</h1>
 
   {!! Form::model($ad, ['method'=>'PATCH', 'url'=>'ads/'.$ad->id, 'enctype' => 'multipart/form-data', 'files'=>true]) !!}
-    @include('ads.partials.form', ['submitButtonText' => 'Update ad','edit' => true])
+    @include('ads.partials.form', ['submitButtonText' => 'Update Ad','edit' => true])
   {!! Form::close() !!}
 
 @stop

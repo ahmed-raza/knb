0107149f0853405f51e3d74bd3e0f4004ad6a2bf
Alex Roye <alex@creativefaze.com>
2017-08-02 12:12:47 +0500

    Ad create form message for pulling contact info

--- resources/views/ads/create.blade.php
+++ resources/views/ads/create.blade.php
@@ -8,6 +8,9 @@
 
   {!! Form::open(['url'=>route('ads.store'), 'enctype'=>'multipart/form-data', 'files'=>true]) !!}
     @include('ads.partials.form', ['submitButtonText' => 'Post Ad','edit' => false])
+    <div class="row">
+      <div class="col-lg-12"><p>Your contact information will be pulled from your profile.</p></div>
+    </div>
   {!! Form::close() !!}
 
 @stop

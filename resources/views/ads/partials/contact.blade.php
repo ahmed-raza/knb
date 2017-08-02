7e9e30a963077f4447565b42126999695a7dbfdc
Alex Roye <alex@creativefaze.com>
2017-08-02 17:17:00 +0500

    Input field error class

--- resources/views/ads/partials/contact.blade.php
+++ resources/views/ads/partials/contact.blade.php
@@ -2,15 +2,15 @@
 
   <div class="form-group">
     {!! Form::label('name') !!}
-    {!! Form::text('name', null, ['class'=>'form-control']) !!}
+    {!! Form::text('name', null, ['class'=>$errors->has('name') ? 'form-control has-error' : 'form-control']) !!}
   </div>
   <div class="form-group">
     {!! Form::label('phone') !!}
-    {!! Form::text('phone', null, ['class'=>'form-control']) !!}
+    {!! Form::text('phone', null, ['class'=>$errors->has('phone') ? 'form-control has-error' : 'form-control']) !!}
   </div>
   <div class="form-group">
     {!! Form::label('message') !!}
-    {!! Form::textarea('message', null, ['class'=>'form-control ckeditor']) !!}
+    {!! Form::textarea('message', null, ['class'=>$errors->has('message') ? 'ckeditor has-error' : 'ckeditor']) !!}
   </div>
   <div class="form-group">
     {!! Form::submit('Send', ['class'=>'btn btn-success btn-block']) !!}

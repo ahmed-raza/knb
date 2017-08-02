8febe9fcf2a17b394d35083ed7d94392205fdbed
Alex Roye <alex@creativefaze.com>
2017-08-02 16:59:10 +0500

    About page view

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,9 @@
+@extends('app')
+
+@section('title', 'About')
+@section('content')
+
+  <h1>About</h1>
+  <p>Ea cillum ut in mollit exercitation deserunt minim in irure elit ea est adipisicing fugiat non ea aliqua consequat qui non id sint irure voluptate eu commodo velit consequat consectetur pariatur non id irure mollit id voluptate ullamco ad veniam dolore aliqua qui reprehenderit adipisicing reprehenderit id et id dolore anim ut velit qui veniam dolore cupidatat elit enim veniam magna ad amet culpa irure qui irure fugiat pariatur consequat nostrud dolore amet labore esse in id anim enim dolor enim anim aliquip dolore id ullamco incididunt nostrud ea cupidatat nisi veniam cillum laboris ex elit occaecat sit ad ut pariatur incididunt nulla ex consectetur sunt voluptate ex incididunt laboris proident proident elit anim tempor consectetur nulla et exercitation dolor ut cillum dolor labore adipisicing ea velit enim deserunt et dolore minim tempor laboris ad nisi aliquip reprehenderit non laboris ex minim velit qui esse irure aliqua sed minim eiusmod dolor voluptate elit in eiusmod nostrud commodo nostrud voluptate excepteur sunt id pariatur aute do voluptate nulla ea minim in cillum velit labore in sunt.</p>
+
+@stop

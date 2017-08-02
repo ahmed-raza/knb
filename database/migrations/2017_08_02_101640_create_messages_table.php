4c31c3bbd30514dcb9567f7860d8248f56731beb
Alex Roye <alex@creativefaze.com>
2017-08-02 17:07:21 +0500

    Message migration

---
+++ database/migrations/2017_08_02_101640_create_messages_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('ad_id')->unsigned();
+            $table->string('name');
+            $table->string('phone')->unique();
+            $table->text('message');
+            $table->rememberToken();
+            $table->timestamps();
+
+            $table->foreign('ad_id')
+                    ->references('id')
+                    ->on('ads')
+                    ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}

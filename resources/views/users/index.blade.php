727bf98d135b97238d8cb70710b7ddb2b365026a
Alex Roye <alex@creativefaze.com>
2017-08-02 17:06:03 +0500

    Shown user ads with their messages

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -6,6 +6,15 @@
 
   <h1>{{ $user->name }}</h1>
 
-  <strong>Email:</strong> {!! $user->email !!}
+  <p><strong>Email:</strong> {!! $user->email !!}</p>
+  <p><strong>Phone:</strong> {!! $user->phone !!}</p>
+
+  @unless($user->ads->isEmpty())
+    <ul>
+      @foreach($user->ads as $userAds)
+        <li><strong><a href="{{ route('ads.show', $userAds->id) }}">{{ $userAds->title }}</a></strong><span> (messages: {{ count($userAds->messages) }})</span></li>
+      @endforeach
+    </ul>
+  @endunless
 
 @stop

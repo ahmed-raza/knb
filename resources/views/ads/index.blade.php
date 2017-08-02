a8ad832c2446362991017eab866c9d562365cbb3
Alex Roye <alex@creativefaze.com>
2017-08-02 17:04:26 +0500

    Index structure with or without images

--- resources/views/ads/index.blade.php
+++ resources/views/ads/index.blade.php
@@ -7,10 +7,12 @@
   <hr>
   @foreach($ads as $ad)
     <div class="row">
-      <div class="col-lg-4">
-        <img src="data:image/jpeg;base64,{{ base64_encode(CustomHelper::getPath($ad->images[0], 'thumb')) }}" alt="Ad Image">
-      </div>
-      <div class="col-lg-8">
+      @unless($ad->images->isEmpty())
+        <div class="col-lg-4">
+          <img src="data:image/jpeg;base64,{{ base64_encode(CustomHelper::getPath($ad->images[0], 'thumb')) }}" alt="Ad Image">
+        </div>
+      @endunless
+      <div class="{!! ($ad->images->isEmpty() ? 'col-lg-12' : 'col-lg-8') !!}">
         <h2><a href="{{ route('ads.show', $ad->id) }}">{{ $ad->title }}</a></h2>
       </div>
     </div>

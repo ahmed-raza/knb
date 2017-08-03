99acd0cafb3ef03cb2fb1eea84651884d6cad124
Alex Roye <alex@creativefaze.com>
2017-08-03 11:10:58 +0500

    Ad index structure adjustments

--- resources/views/ads/index.blade.php
+++ resources/views/ads/index.blade.php
@@ -14,6 +14,11 @@
       @endunless
       <div class="{!! ($ad->images->isEmpty() ? 'col-lg-12' : 'col-lg-8') !!}">
         <h2><a href="{{ route('ads.show', $ad->id) }}">{{ $ad->title }}</a></h2>
+        <p><em>{{ $ad->created_at->diffForHumans() }}</em></p>
+        <p><strong>Price: </strong>{{ $ad->car_price }} PKR</p>
+        <p><strong>Make: </strong>{{ $ad->car_make }}</p>
+        <p><strong>Model: </strong>{{ $ad->car_model }}</p>
+        {!! str_limit($ad->description, 350) !!}
       </div>
     </div>
   @endforeach

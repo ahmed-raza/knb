d4098d6310a7062b29d9d13e16d06afb0fd0df42
Alex Roye <alex@creativefaze.com>
2017-08-04 10:21:35 +0500

    Contact submission store and sending email to admin

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,10 +3,41 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\ContactRequest;
+use App\Contact;
+use Mail;
 
 class ContactController extends Controller
 {
   public function index(){
     return view('pages.contact');
+  }
+  public function store(ContactRequest $request){
+    $contact = Contact::create($request->all());
+    if ($contact) {
+      $this->sendMail($request);
+    }
+    return redirect(route('contact.index'))->with('message', 'Thank you for your submission, someone from our team will contact you shortly.');
+  }
+  private function sendMail($request){
+    $from = $request->get('email');
+    $name = $request->get('name');
+    $subject = $request->get('subject');
+    $mail_message = $request->get('message');
+    $data = [
+      'from'=>$from,
+      'name'=>$name,
+      'subject'=>$subject,
+      'mail_message'=>$mail_message,
+    ];
+    $mail = Mail::send('mail.contact', $data, function($message) use ($from, $subject){
+      $message->from($from);
+      $message->to('ahmed.raza@square63.com')->subject($subject);
+    });
+    if ($mail) {
+      return true;
+    }else{
+      return false;
+    }
   }
 }

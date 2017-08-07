778737feef551a54345e3a4dd5fe386d7083b62d
Alex Roye <alex@creativefaze.com>
2017-08-07 13:14:12 +0500

    Changed route from POST to GET

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::group(['prefix'=>'user/{id}'], function(){
   Route::get('logout', ['as'=>'user.logout', 'uses'=>'UsersController@logout']);
 });
 
-Route::post('ads/search', ['as'=>'ads.search', 'uses'=>'AdsController@search']);
+Route::get('ads/search', ['as'=>'ads.search', 'uses'=>'AdsController@index']);
 Route::post('ads/{ad}/send-message', ['as'=>'ads.message', 'uses'=>'AdsController@adsMessage']);
 Route::get('ads/{ad}/delete', ['as'=>'ads.delete', 'uses'=>'AdsController@delete']);
 Route::resource('ads', 'AdsController');

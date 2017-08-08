930d020f93a01f914ac953075d3643de819ef99b
Alex Roye <alex@creativefaze.com>
2017-08-08 12:05:24 +0500

    Pic and Bio fillables

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'phone', 'password', 'rank',
+        'name', 'email', 'phone', 'password', 'pic', 'bio', 'rank',
     ];
 
     /**

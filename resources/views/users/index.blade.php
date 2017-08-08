0905d0d6d258a5c717dc6567860276de0e3f6345
Alex Roye <alex@creativefaze.com>
2017-08-08 11:01:07 +0500

    User messages in bootstrap modal

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -13,12 +13,12 @@
 
 @section('sidebar')
 
-  <fieldset>
+  <fieldset class="user-messages">
     <legend>My Ads</legend>
     @unless($user->ads->isEmpty())
       <div class="list-group">
-        @foreach($user->ads as $userAds)
-          <a href="{{ route('ads.show', $userAds->id) }}" class="list-group-item">{{ $userAds->title }}<span title="Messages" class="badge">{{ count($userAds->messages) }}</span></a>
+        @foreach($user->ads as $ad)
+          <a href="{{ route('ads.show', $ad->id) }}" class="list-group-item">{{ $ad->title }}</a>
         @endforeach
       </div>
     @endunless
@@ -28,8 +28,43 @@
     <legend>Messages</legend>
     @unless($user->messages->isEmpty())
       <div class="list-group">
-        @foreach($user->messages as $userMessages)
-          <a class="list-group-item" href="#"><strong>{{ $userMessages->name }}</strong></a>
+        @foreach($user->messages as $key => $message)
+          <a class="list-group-item" href="#" data-toggle="modal" data-target="#message-{{ $key }}"><strong>{{ $message->name }}</strong></a>
+          <div class="modal fade" id="message-{{ $key }}" role="dialog">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <button type="button" class="close" data-dismiss="modal">&times;</button>
+                  <h4 class="modal-title">{{ $message->name }}</h4>
+                </div>
+                <div class="modal-body">
+                  <em>{{ $message->created_at->diffForHumans() }}</em>
+                  {!! $message->message !!}
+                  <p><strong>Phone: </strong>{{ $message->phone }}</p>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+              </div>
+              
+            </div>
+          </div>
+          <div class="modal fade" id="myModal" role="dialog">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <button type="button" class="close" data-dismiss="modal">&times;</button>
+                  <h4 class="modal-title">Modal Header</h4>
+                </div>
+                <div class="modal-body">
+                  <p>Some text in the modal.</p>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+              </div>
+            </div>
+          </div>
         @endforeach
       </div>
     @endunless

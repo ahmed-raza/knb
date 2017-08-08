cd75e5db001836ea08bbd00e2a49474efff11fea
Alex Roye <alex@creativefaze.com>
2017-08-08 15:33:21 +0500

    Removed admin from normal navbar dropdown

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -27,9 +27,6 @@
             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
             <ul class="dropdown-menu">
               <li><a href="{{ route('user.profile') }}">Profile</a></li>
-              @if(Auth::user()->rank == 'admin')
-              <li><a href="{{ route('admin.dashboard') }}">Admin</a></li>
-              @endif
               <li role="separator" class="divider"></li>
               <li><a href="{{ route('user.logout', Auth::user()->id) }}">Logout</a></li>
             </ul>
